e44e0e1788154feb66a2d128c0267ce2ee036467
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2025-05-28 22:00:36 +0800

    feat: 修改版面，刪除不需要的部份

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 
 interface ImageCompareSource {
   before: string;
@@ -8,11 +7,6 @@ interface ImageCompareSource {
 
 const imageCompareSource: ImageCompareSource[] = [];
 
-// imageCompareSource.push({
-//   before: "B01.jpg",
-//   after: "A01.jpg"
-// });
-
 imageCompareSource.push({
   before: "IMG_8870.jpg",
   after: "IMG_8999.jpg"
@@ -20,22 +14,15 @@ imageCompareSource.push({
 </script>
 
 <template>
-  <main>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button label="Submit" />
-    <div v-for="(item, index) in imageCompareSource" :key="index">
-      <!-- 這個元件的左右邊跟我理解的相反 -->
-      <ImageCompare class="shadow-lg rounded-2xl">
-        <template #left>
-          <img :src="item.after" />
-        </template>
-        <template #right>
-          <img :src="item.before" />
-        </template>
-      </ImageCompare>
-    </div>
-    <TheWelcome />
-  </main>
+  <div class="w-full" v-for="(item, index) in imageCompareSource" :key="index">
+    <!-- 這個元件的左右邊跟我理解的相反 -->
+    <ImageCompare class="shadow-lg rounded-2xl">
+      <template #left>
+        <img :src="item.after" />
+      </template>
+      <template #right>
+        <img :src="item.before" />
+      </template>
+    </ImageCompare>
+  </div>
 </template>
