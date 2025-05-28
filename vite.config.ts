75252c403aabc2daf0b5437e09a424d5eea5f272
Hong, Jian-Ching <allyusd@yahoo.com.tw>
2025-05-28 20:43:07 +0800

    ci: 設定子目錄

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/home-organizer/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     vue(),
     vueJsx(),
